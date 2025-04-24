4db57a8ecbfac117c05b26bf2a21ea9e759b7bdc
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-23 20:23:00 -0400

    Update CLI_Commands.py

--- Networking Tools/Subdomain Enumerator/CLI_Commands.py
+++ Networking Tools/Subdomain Enumerator/CLI_Commands.py
@@ -1,9 +1,9 @@
 import argparse
 
-# Intializing Argument
-
-parser = argparse.ArgumentParser(description="A Program to find available subdomains via lists or APIs")
-
-# Command to add a user chosen wordlist
-
-parser.add_argument('-wordlist', help="Enter in a wordlist to scan specific subdomains")
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Subdomain Enumeration Tool")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-d", "--default", action="store_true", help="Use default prefix list")
+    group.add_argument("-w", "--wordlist", type=str, help="Path to custom wordlist file")
+    parser.add_argument("domain", help="Target domain name")
+    return parser.parse_args()
