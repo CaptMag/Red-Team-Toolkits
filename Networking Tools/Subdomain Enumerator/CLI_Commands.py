10ea8fbc5107953bfc842067e94f1b2af60ddadf
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-25 20:04:40 -0400

    Delete Networking Tools/Subdomain Enumerator/CLI_Commands.py

--- Networking Tools/Subdomain Enumerator/CLI_Commands.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Subdomain Enumeration Tool")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-d", "--default", action="store_true", help="Use default prefix list")
-    group.add_argument("-w", "--wordlist", type=str, help="Path to custom wordlist file")
-    parser.add_argument("domain", help="Target domain name")
-    return parser.parse_args()
