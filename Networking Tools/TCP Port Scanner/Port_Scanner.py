c6dac11a2e2a67edfe20c64a8b75b048a1a0567c
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-05-04 19:44:26 -0400

    Update Port_Scanner.py

--- Networking Tools/TCP Port Scanner/Port_Scanner.py
+++ Networking Tools/TCP Port Scanner/Port_Scanner.py
@@ -3,48 +3,52 @@ import os
 
 def TCP_Port_Scanner(portscanner_path, combined_file):
 
+    if not os.path.exists(portscanner_path): # Checks to see if argparse path exists in main.py
+        print(f"Error! File does not exist: {portscanner_path}")
+        return
+
     try:
         destination = socket.gethostbyname(combined_file) # Gets the hostname from the combined file in either Wordlist.py or Subdomain_Enumeration.py
-    except socket.gaierror:
-        print(f"Error! Could not resolve hot {destination}")
+    except socket.gaierror as g:
+        print(f"Error! Could not resolve host {g}")
+        return
 
-        ports = []
+    with open("Port_Results.txt", "w", encoding="utf-8") as results_file: # Creates results file for ports
+
         successful_ports = []
         unsuccessful_ports = []
 
 
-        with open("Port_Results.txt", "w", encoding="utf-8") as results_file: # Creates results file for ports
-
-
-            for port in range(1, 1001): # Goes through from port 1 to 10000
-                    
-
-                    try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Establishes the beginning of a socket
-                        print("TCP Socket has been established!")
-                    except socket.error as e:
-                        print(f"Error! Socket Not Established {e}")
-
-
-                    s.connect((destination, port)) # Checks if a port is online based on the desired DNS
-                    ports.append(f"Available Ports Online: {ports}")
-
-            if ports:
-                successful_ports.append(combined_file)
-                result_line = f"Port Is Available! {port}"
-                results_file.write(result_line)
-            else:
-                 unsuccessful_ports.append(combined_file)
-                 result_line = f"Port Is Unavailable! {port}"
-                 results_file.write(result_line)
+        for port in range(1, 65535): # Goes through from port 1 to 10000
                 
 
+                try:
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Establishes the beginning of a socket
+                except socket.error as e:
+                    print(f"Error! Socket Not Established {e}")
 
-            if not os.path.exists(portscanner_path): # Checks to see if argparse path exists in main.py
-                print(f"Error! File does not exist: {portscanner_path}")
-                return
+                    
+                try:
+                    s.settimeout(0.1)
+                    s.connect((destination, port)) # Checks if a port is online based on the desired DNS
+                    successful_ports.append(port)
+                except socket.error as e:
+                     unsuccessful_ports.append(port)
+                finally:
+                    s.close()
             
+
+        for port in successful_ports:
+            result_line = f"Port Is Available! {port}"
+            results_file.write(result_line + "\n")
+
+        if port in unsuccessful_ports:
+            result_line = f"Port Is Unavailable! {port}"
+            results_file.write(result_line + "\n")
+                
+            
+        
     print(f"\nSummary:")
-    print(f"Total subdomains checked: {len(ports) + len(ports)}")
-    print(f"Subdomains Available: {len(successful_ports)}")
-    print(f"Subdomains Not Available: {len(unsuccessful_ports)}")
+    print(f"Total Ports Checked: {len(successful_ports) + len(unsuccessful_ports)}")
+    print(f"Ports Open: {len(successful_ports)}")
+    print(f"Ports Not Open: {len(unsuccessful_ports)}")
