80866b94541232e0bbb0ac584bd6443ea513ce09
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-05-15 19:06:28 -0400

    Added OS  Detection using TTL TCP Packets to TCP_Scanner.py

--- Networking Tools/TCP Port Scanner/TCP_Scanner.py
+++ Networking Tools/TCP Port Scanner/TCP_Scanner.py
@@ -1,12 +1,14 @@
 import socket
 import ssl
 import concurrent.futures
+from scapy.all import *
 
 
 def TCP_Scanner(destination, resolved_ip, port):
 
 
     # Lists for both open and closed ports
+    open_ports = []
     closed_ports = []
 
     request = b"GET / HTTP/1.0\r\n\r\n" # sends HTTP GET Request to server
@@ -31,6 +33,7 @@ def TCP_Scanner(destination, resolved_ip, port):
         s.sendall(request) # sends data to server
 
         request = s.recv(4096) # receives answer from server
+        open_ports.append(port)
         print(f"[OPEN PORT]: {port} | [SERVICE]: {service_name} | [BANNER]: {request.decode()}") # prints open port and service name and banner
     
 
@@ -67,16 +70,31 @@ def threadpoolexecutor(destination):
             future_to_port = {executor.submit(TCP_Scanner, destination, resolved_ip, port): port for port in range (1, 65536)} # goes through TCP function and through the range of ports
 
             for future in concurrent.futures.as_completed(future_to_port): # For loops the future_to_port through all the ports in order to scan faster
-                    port = future_to_port[future]
-                    result = future.result()
-                    if result:
-                        open_ports.append(result[0]) # Append results
+                    # port = future_to_port[future]
+                    results = future.result()
+                    if results:
+                        open_ports.append(results) # Append results
 
             # Total open and closed ports
             print("\nScan results:")
             print(f"Open ports: {sorted(open_ports)}")
             print(f"Closed ports: {65535 - len(open_ports)}")
 
+def os_fingerprinting(destination, open_ports):
+    
+    data_sent = IP(dst=destination)/TCP(dport=open_ports, flags="S", seq=102) # Sends a TCP Packet to a specific destination
+    result = sr1(data_sent, timeout=2, verbose=0)
+
+
+    # Checks the Time Till Live for a packet, and depending on the answer, prints its likely operating system
+    if result.ttl <= 64:
+        print(f"[+] TTL from {destination} | Response Time {result.ttl} | Likely a Linux Machine")
+    elif result.ttl == 128:
+        print(f"[+] TTL from {destination} | Response Time {result.ttl} | Likely a Windows Machine")
+    elif result.ttl == 255:
+        print(f"[+] TTL from {destination} | Response Time {result.ttl} | Likely a Cisco Machine")
+    else:
+         print(f"[-] Error! Could not find TTL for {destination}")
          
 
 if __name__ == "__main__":
@@ -84,3 +102,4 @@ if __name__ == "__main__":
     destination = input("Enter in a domain name or IPv4 Address: ") # Asks a user for a specified domain name or IPv4 address
 
 threadpoolexecutor(destination)
+os_fingerprinting(destination, 80)
